ce2be570f39cab93939e0428e9cab967f15cc835
kennyzx <ikennyzx@gmail.com>
2017-11-05 23:34:00 +0800

    Install CCRewriter, and use Code Contracts. It is of little use

---
+++ CodeContracts/CodeContracts/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+
+namespace CodeContracts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertStringToInteger("");
+        }
+
+        public static int ConvertStringToInteger(string input)
+        {
+            Contract.Requires<ArgumentException>(!string.IsNullOrEmpty(input));
+            return 0;
+        }
+    }
+}
